2502a19a2b3526461ea1d84192d41af6cd3333f7
Josiah Lau <josiah.lau@teamaol.com>
2016-04-15 17:10:30 -0400

    First commit

---
+++ swagger2chakram.js
@@ -0,0 +1,108 @@
+// convert swagger json to basic tests
+
+var _ = require('lodash');
+var fs = require('fs');
+var stream = fs.createWriteStream('test.js');
+var http = require('http');
+var es6 = require('es6-promise').polyfill();
+var fetch = require('isomorphic-fetch');
+var url = //url to retrieve swagger api doc
+
+stream.write('var chakram = require(\'chakram\'),\n')
+stream.write('    expect  = chakram.expect,\n')
+stream.write('    moment  = require(\'moment\'),\n')
+stream.write('    _       = require(\'lodash\'),\n')
+stream.write('    config  = require(\'./config\'),\n')
+stream.write('    baseUrl = config.base_url;\n\n')
+
+
+function createTest(uri, reqType, summary) {
+  stream.write('describe(\"' + summary + '\", function() {\n')
+  stream.write('  before(function() {\n')
+  if (reqType == 'get') {
+    stream.write('    request = chakram.'+reqType+'(baseUrl + \'' + uri+'\', {\'headers\': {\'Accept\': "application/json; version=1.0"}})\n')
+    stream.write('  });\n\n')
+    stream.write('  it(\"should return 200 on success\", function () {\n')
+    stream.write('    return expect(request).to.have.status(200);\n')
+    stream.write('  });\n')
+    stream.write('  it(\"should return JSON content type and server headers\", function () {\n')
+    stream.write('    expect(request).to.have.header(\"server\");\n')
+    stream.write('    expect(request).to.have.header(\"content-type\", \"application/json\");\n')
+    stream.write('    return chakram.wait();\n')
+  }
+  else if (reqType == 'post') {
+    stream.write('    request = chakram.'+reqType+'(baseUrl + \'' + uri+'\',\n')
+    stream.write('       true,\n')
+    stream.write('       {\n')
+    stream.write('       \'headers\': {\n')
+    stream.write('          \'Accept\': "application/json; version=1.0"\n')
+    stream.write('       },\n')
+    stream.write('       \'body\': {\n')
+    stream.write('\n')
+    stream.write('       }\n')
+    stream.write('    })\n')
+    stream.write('  });\n\n')
+    stream.write('  it(\"should return 201 on success\", function () {\n')
+    stream.write('    return expect(request).to.have.status(201);\n')
+    stream.write('  });\n')
+    stream.write('  it(\"should return JSON content type and server headers\", function () {\n')
+    stream.write('    expect(request).to.have.header(\"server\");\n')
+    stream.write('    expect(request).to.have.header(\"content-type\", \"application/json\");\n')
+    stream.write('    return chakram.wait();\n')
+  }
+  else if (reqType == 'put') {
+    stream.write('    request = chakram.'+reqType+'(baseUrl + \'' + uri+'\',\n')
+    stream.write('       true,\n')
+    stream.write('       {\n')
+    stream.write('       \'headers\': {\n')
+    stream.write('          \'Accept\': "application/json; version=1.0"\n')
+    stream.write('       },\n')
+    stream.write('       \'body\': {\n')
+    stream.write('\n')
+    stream.write('       }\n')
+    stream.write('    })\n')
+    stream.write('  });\n\n')
+    stream.write('  it(\"should return 200 on success\", function () {\n')
+    stream.write('    return expect(request).to.have.status(200);\n')
+    stream.write('  });\n')
+    stream.write('  it(\"should return JSON content type and server headers\", function () {\n')
+    stream.write('    expect(request).to.have.header(\"server\");\n')
+    stream.write('    expect(request).to.have.header(\"content-type\", \"application/json\");\n')
+    stream.write('    return chakram.wait();\n')
+  }
+  else if (reqType == 'delete') {
+    stream.write('    request = chakram.'+reqType+'(baseUrl + \'' + uri+'\',\n')
+    stream.write('       true,\n')
+    stream.write('       {\n')
+    stream.write('       \'headers\': {\n')
+    stream.write('          \'Accept\': "application/json; version=1.0"\n')
+    stream.write('       },\n')
+    stream.write('    })\n')
+    stream.write('  });\n\n')
+    stream.write('  it(\"should return 204 on success\", function () {\n')
+    stream.write('    return expect(request).to.have.status(204);\n')
+    stream.write('  });\n')
+    stream.write('  it(\"should return JSON content type and server headers\", function () {\n')
+    stream.write('    expect(request).to.have.header(\"server\");\n')
+    stream.write('    return chakram.wait();\n')
+  }
+  stream.write('  });\n')
+  stream.write('});\n\n')
+}
+
+fetch(url)
+.then(function(response) {
+
+  if (response.status >= 400) {
+    throw new Error("Bad Response");
+  }
+  return response.json();
+}).then(function(res) {
+  _.forIn(res.paths, function(methods, endpoint) {
+    _.forIn(methods, function(params, method) {
+      createTest(endpoint, method, params.summary, params);
+    });
+  });
+
+  stream.end();
+});
